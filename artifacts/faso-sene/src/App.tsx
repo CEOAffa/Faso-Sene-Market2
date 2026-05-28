dca692ba8796cd2df6200eb0bee9d4ab18aba004
CEOAffa <abdoudianeafa@gmail.com>
2026-05-29 00:03:29 +0200

    Update App.tsx

--- artifacts/faso-sene/src/App.tsx
+++ artifacts/faso-sene/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+        <WouterRouter base="">
           <Router />
         </WouterRouter>
         <Toaster />
